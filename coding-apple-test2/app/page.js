290b3db8ffff87b2126541db01cb50728caa7522
junsung5779 <junsung5779@gmail.com>
2023-11-18 14:24:14 +0900

    feat: mongoDB 연결하기

--- coding-apple-test2/app/page.js
+++ coding-apple-test2/app/page.js
@@ -1,7 +1,22 @@
+import { connectDB } from '/util/database.js'
 
-
-export default function Home() {
+export default async function Home() {
+  // Mongode connect
+  const db = (await connectDB).db('coding-apple-test2')
+  let result = await db.collection('post').find().toArray()
+  /**
+   * client component 내의 code는 user browser로 전달되기 때문에
+   * 해당 페이지를 사용하는 모든 user 들이 DB I/O code를 볼 수 있다.
+   * 따라서 DB I/O code는 server component 안에서만 쓰자.
+   */
+  console.log(result)
   return (
-    <div></div>
+    <main>
+      <div>
+        {result[0].title}
+        <br />
+        {result[0].content}
+      </div>
+    </main>
   )
 }
