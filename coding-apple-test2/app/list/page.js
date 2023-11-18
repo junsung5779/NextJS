c0e75e78b4dee83e1d5d6b0af9e5e67c45682145
junsung5779 <junsung5779@gmail.com>
2023-11-18 20:29:32 +0900

    feat:

--- coding-apple-test2/app/list/page.js
+++ coding-apple-test2/app/list/page.js
@@ -1,4 +1,5 @@
 import { connectDB } from '@/util/database'
+import Link from 'next/link'
 
 export default async function List() {
   const db = (await connectDB).db('coding-apple-test2')
@@ -9,9 +10,11 @@ export default async function List() {
     <div className="list-bg">
       {result.map((e, idx) => {
         return (
-          <div className="list-item">
-            <h4>{result[idx].title}</h4>
-            <p>{result[idx].content}</p>
+          <div className="list-item" key={idx}>
+            <Link href={`/detail/${result[idx]._id}`}>
+              <h4>{result[idx].title}</h4>
+              <p>{result[idx].content}</p>
+            </Link>
           </div>
         )
       })}
